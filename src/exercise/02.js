a272a9ec1f121e8fc896c870219ab99e3e1ac4fa
Phil Fuster <pafus23@protonmail.com>
2024-11-20 10:47:23 -0500

    Exercise 2. Extra Credit 2. React.useEffect and dependency arrays to limit when useEffect callbacks run.

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,21 +3,23 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
+function Greeting({initialName = '', initialToggle = false}) {
   const [name, setName] = React.useState(
     () => window.localStorage.getItem('name') ?? initialName,
   )
-
   React.useEffect(() => {
     window.localStorage.setItem('name', name)
-  })
+    console.log(
+      'use effect for setting name into local storage ran during re-render.',
+    )
+  }, [name])
 
   function handleChange(event) {
     setName(event.target.value)
   }
   return (
     <div>
-      <form>
+      <form onSubmit={event => event.preventDefault()}>
         <label htmlFor="name">Name: </label>
         <input value={name} onChange={handleChange} id="name" />
       </form>
@@ -27,7 +29,7 @@ function Greeting({initialName = ''}) {
 }
 
 function App() {
-  return <Greeting initialName="Big Dawg" />
+  return <Greeting initialName="Big Dawg" initialToggle={false} />
 }
 
 export default App
